3ce212355652854924b51bf6ebad633990387c18
Nya Tyus <nyastyus@gmail.com>
2024-01-01 18:48:57 -0500

    add navigation

---
+++ src/components/Navigation.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
+import "../styles/Nav.css";
+
+function Navigation() {
+  return (
+    <Navbar expand="lg" className="nav-bg">
+      <Container>
+        <Navbar.Brand className="nav-name-text">Nya Tyus</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto nav-text">
+            <Nav.Link as={Link} style={{ textDecoration: "none" }} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} style={{ textDecoration: "none" }} to="/about">
+              About
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              style={{ textDecoration: "none" }}
+              to="/portfolio"
+            >
+              Portfolio
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              style={{ textDecoration: "none" }}
+              to="https://www.google.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Resume
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Navigation;
