e85bffcc06400049f82c1658020693efedf78c5f
Nya Tyus <nyastyus@gmail.com>
2024-01-06 04:48:25 -0500

    add about section

---
+++ src/pages/About.js
@@ -0,0 +1,63 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <h1>ABOUT ME</h1>
+      <div>
+        Hey there! I'm Nya, an Atlanta, GA native who's always had a love for
+        numbers and science. I have a solid foundation in biology and a
+        comprehensive Full Stack Web Development certificate. My journey has
+        been a fusion of scientific exploration and coding expertise, making me
+        a dynamic candidate for data analyst roles.
+      </div>
+      <div>
+        Armed with a B.S. in Biology from Clayton State University, I've
+        ventured into the realm of data analysis through an intensive Full Stack
+        Web Development bootcamp at the Georgia Institute of Technology. My
+        analytical skills, honed in the biological sciences, have seamlessly
+        translated into the world of data, where I've mastered statistical
+        analysis using tools such as Excel and Google Sheets functions.
+      </div>
+      <div>
+        My technical toolkit includes SQL, where I'm well-versed in joins and
+        queries, as well as JavaScript and some Python. This proficiency extends
+        to extracting meaningful insights from complex datasets, a skill crucial
+        for any data analyst role.
+      </div>
+      <div>
+        The three-month bootcamp not only showcased my ability to swiftly learn
+        and apply new concepts but also highlighted my collaborative nature.
+        With over 20 homework assignments and three collaborative projects, I've
+        honed my skills in working effectively within a team to achieve
+        data-driven goals.
+      </div>
+      <div>
+        My unique combination of biology and web development skills provides me
+        with a holistic perspective on data analysis. This interdisciplinary
+        approach allows me to approach complex challenges with a blend of
+        scientific rigor and technological efficiency.
+      </div>
+      <div>
+        Outside the realm of data, you'll find me immersed in a good book,
+        experimenting with new recipes in the kitchen, or indulging in my love
+        for travel. These passions not only enrich my life but also contribute
+        to my ability to think creatively and approach problem-solving with an
+        open mind.
+      </div>
+      <div>
+        Motivated by a deep interest in healthcare, I am eager to contribute my
+        skills to the dynamic field of healthcare analytics. My background and
+        knowledge position me as a candidate ready to leverage data to drive
+        insights and innovations in healthcare environments.
+      </div>
+      <div>
+        If you're seeking a data analyst with a diverse skill set, combining
+        biological insights with robust technical capabilities, I'm excited to
+        explore potential collaborations. Dive into my portfolio, and let's
+        connect to discuss how I can contribute to elevating your organization's
+        data strategy.
+      </div>
+    </div>
+  );
+}
