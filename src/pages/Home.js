d61a4db40d8edc137842b78da6bbf0a27800b73d
Nya Tyus <nyastyus@gmail.com>
2024-01-06 03:31:50 -0500

    add headshot to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import "../styles/Home.css";
 
 export default function Home() {
   return (
-    <div className="home-bg home-cols">
-      <div className="home-left">
-        <div>image here</div>
-      </div>
-      <div className="home-right">
-        <div>info here</div>
-      </div>
-    </div>
+    <Container
+      fluid
+      className="home-bg d-flex align-items-center justify-content-center"
+    >
+      <Row className="">
+        <Col className="">
+          <img
+            className="home-img mx-auto d-block"
+            src="/images/headshot2.jpeg"
+            alt=""
+          />
+        </Col>
+        <Col className="text-center align-self-center">
+          <div>Nya Tyus</div>
+          <div> Data Analyst </div>
+          <div>Biology, B.S.</div>
+          <div>Full Stack Web Development Bootcamp Graduate</div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
