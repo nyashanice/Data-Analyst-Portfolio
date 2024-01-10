29fc157297f78f36f76c58d1032226b055951990
Nya Tyus <nyastyus@gmail.com>
2024-01-10 03:32:52 -0500

    fix homepage media query flexbox

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,19 +7,43 @@ import "../styles/Home.css";
 
 export default function Home() {
   return (
-    <Container
-      fluid
+    // <Container
+    //   fluid="sm"
+    //   className="home-bg d-flex align-items-center justify-content-center"
+    // >
+    //   <Row className="home-row">
+    //     <Col className="">
+    //       <img
+    //         className="home-img mx-auto d-block"
+    //         src="../Data-Analyst-Portfolio/images/headshot2.jpeg"
+    //         alt=""
+    //       />
+    //     </Col>
+    //     <Col className="text-center align-self-center">
+    //       <div className="home-name">NYA TYUS</div>
+    //       <span className="home-title m-2 p-2">
+    //         {" "}
+    //         Data Analyst, Web Developer{" "}
+    //       </span>
+    //       <div className="home-txt m-2">Bachelor of Science in Biology</div>
+    //       <div className="home-txt">
+    //         Full Stack Web Development Bootcamp Graduate
+    //       </div>
+    //     </Col>
+    //   </Row>
+    // </Container>
+    <div
       className="home-bg d-flex align-items-center justify-content-center"
     >
-      <Row className="">
-        <Col className="">
+      <div className="home-row">
+        <div className="">
           <img
             className="home-img mx-auto d-block"
             src="../Data-Analyst-Portfolio/images/headshot2.jpeg"
             alt=""
           />
-        </Col>
-        <Col className="text-center align-self-center">
+        </div>
+        <div className="text-center align-self-center">
           <div className="home-name">NYA TYUS</div>
           <span className="home-title m-2 p-2">
             {" "}
@@ -29,8 +53,8 @@ export default function Home() {
           <div className="home-txt">
             Full Stack Web Development Bootcamp Graduate
           </div>
-        </Col>
-      </Row>
-    </Container>
+        </div>
+      </div>
+    </div>
   );
 }
